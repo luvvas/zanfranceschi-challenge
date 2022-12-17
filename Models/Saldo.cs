7dca32678cda63ccb6a16ad7ab2e2e57e030b163
luvvas <lucasmachado1@ufpr.br>
2022-12-17 01:12:09 -0300

    feat: add Saldo model

---
+++ Models/Saldo.cs
@@ -0,0 +1,10 @@
+namespace zanfranceschi_challenge.Models
+{
+  public class Saldo
+  {
+    public Guid Id { get; set; } = Guid.Empty;
+    public decimal saldo { get; set; } = 0;
+    public DateTime de { get; set; } = DateTime.UtcNow;
+    public Cliente Cliente { get; set; } = new Cliente();
+  }
+}
