f544465998764b49d981ea719cb2d6b3702f9d5e
luvvas <lucasmachado1@ufpr.br>
2022-12-17 01:35:46 -0300

    feat: add CreateClienteAndSaldo migration

---
+++ Migrations/20221217043245_CreateClienteAndSaldo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace zanfranceschichallenge.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateClienteAndSaldo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Saldos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    saldo = table.Column<decimal>(type: "numeric", nullable: false),
+                    de = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Saldos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    nome = table.Column<string>(type: "text", nullable: false),
+                    SaldoId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clientes_Saldos_SaldoId",
+                        column: x => x.SaldoId,
+                        principalTable: "Saldos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_SaldoId",
+                table: "Clientes",
+                column: "SaldoId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Saldos");
+        }
+    }
+}
